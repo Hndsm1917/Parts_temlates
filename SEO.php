f3355c177c6cee88f04cb91029347da559d23508
Hndsm1917 <45174726+Hndsm1917@users.noreply.github.com>
2021-04-15 12:00:41 +0300

    Update SEO.php

--- SEO.php
+++ SEO.php
@@ -13,17 +13,31 @@
 
                    
         <----------- Remove active link ------------- >             
-       
-        /wp-includes/class-walker-nav-menu.php // line 180:
-              if($item->current){
+       /wp-includes/class-walker-nav-menu.php // line 180:
+        <?php 
+               if($item->current){
                      $atts['href']="#";
               } else {
                      $atts['href'] =  $item->url;
-              }
-
+              } 
+         ?>
+        
         /your-file.js/
               document.querySelectorAll("a[href='#']").forEach((element) => {
                element.addEventListener("click", function(event){
                    event.preventDefault()
                })
               })
+        
+               
+        <----------- functions.php 301 Redirect ------------- >   
+               
+       <?php 
+              add_action( 'template_redirect', function() {
+                     if( in_category('portfolio') && !is_category() ){
+                            wp_redirect( 'https://coolkidsblogs.com/portfolio/', 301 );
+                     exit;
+                     }
+              });    
+       ?>      
+      
