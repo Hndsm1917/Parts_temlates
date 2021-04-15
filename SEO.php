9bc896a1f3634f88ff069a035b94b0a7e7b78893
Hndsm1917 <45174726+Hndsm1917@users.noreply.github.com>
2021-04-15 12:02:52 +0300

    Update SEO.php

--- SEO.php
+++ SEO.php
@@ -1,26 +1,26 @@
-       
-       <----------- REMOVE PAGE FROM SEARCH INDEXATION ------------- >
+<?php       
+       // ---------- REMOVE PAGE FROM SEARCH INDEXATION ----------
 
-       <?php if($_SERVER['REQUEST_URI'] == '/hello-world/'){ ?>
+       if($_SERVER['REQUEST_URI'] == '/hello-world/'){ ?>
             <meta name="robots" content="noindex,nofollow" />
        <?php }?>
               
      
               
-       <----------- Fix iphone monobrows ------------- >
+       // <----------- Fix iphone monobrows ------------- >
 
        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 
                    
         <----------- Remove active link ------------- >             
        /wp-includes/class-walker-nav-menu.php // line 180:
-        <?php 
+        
                if($item->current){
                      $atts['href']="#";
               } else {
                      $atts['href'] =  $item->url;
               } 
-         ?>
+         
         
         /your-file.js/
               document.querySelectorAll("a[href='#']").forEach((element) => {
@@ -32,12 +32,12 @@
                
         <----------- functions.php 301 Redirect ------------- >   
                
-       <?php 
+       
               add_action( 'template_redirect', function() {
                      if( in_category('portfolio') && !is_category() ){
                             wp_redirect( 'https://coolkidsblogs.com/portfolio/', 301 );
                      exit;
                      }
               });    
-       ?>      
-      
+    
+?>      
